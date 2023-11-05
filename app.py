002b5cf4399df18d0227c602e7b73439ebdb0447
Neel Shettigar <neelshettigar@gmail.com>
2023-11-05 00:01:53 -0500

    Allowed mobile local testing with ngrok

--- app.py
+++ app.py
@@ -29,4 +29,6 @@ def index():
 #     return jsonify({'result': 'Image processed successfully'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=False)
